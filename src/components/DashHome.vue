01bd2389b477f767c68bdb0e501c344716df1e24
Wes Warriner <fridgehistory@gmail.com>
2021-12-09 20:47:37 -0800

    add barebones Dashboard Home and UserRegistration components

---
+++ src/components/DashHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <button @click="logout">Sign Out</button>
+  </div>
+</template>
+
+<script>
+import { logout } from '../service/firebase.js'
+
+export default {
+    name: 'DashHome',
+    methods: {
+        logout: function () {
+            logout()
+        }
+    }
+};
+</script>
---
+++ src/components/UserRegistration.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="register">
+    <p>Register</p>
+    <input type="text" v-model="email" placeholder="Email">
+    <br>
+    <input type="password" v-model="password" placeholder="Password">
+    <br>
+    <button @click="register">Register</button>
+    <span>
+        <router-link to="/login">login</router-link>
+    </span>
+  </div>
+</template>
+
+<script>
+import { registerUser } from '../service/firebase.js'
+export default {
+  name: "Register",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+      register: function () {
+          registerUser(this.email, this.password)
+      }
+  }
+};
+</script>
+
+<style scoped>
+.register {
+  margin-top: 40px;
+}
+input {
+  width: 40%;
+  padding: 20px;
+  margin: 20px 0;
+}
+button {
+  width: 10%;
+  margin-top: 30px;
+  padding: 20px;
+  cursor: pointer;
+}
+span {
+  display: block;
+  margin-top: 20px;
+  font-size: 11px;
+}
+</style>
