92b1838733fd8aaa4e19490fdc9488eb614e083e
Wes Warriner <fridgehistory@gmail.com>
2021-12-23 12:42:27 -0800

    modify/add to boilerplate and utilize tailwind classes to create prettier (but still basic) login form

--- src/components/DashLogin.vue
+++ src/components/DashLogin.vue
@@ -1,27 +1,93 @@
 <template>
-  <div class="login">
-    <h2>Login</h2>
-    <input
-      v-model="email"
-      type="text"
-      placeholder="Email"
-    ><br>
-    <input
-      v-model="password"
-      type="password"
-      placeholder="Password"
-    ><br>
-    <button @click="login">
-      Sign in
-    </button>
-    <p>
-      <router-link to="/register">
-        Register
-      </router-link>
-    </p>
-    <span>
-      <router-link to="/login">login</router-link>
-    </span>
+  <div
+    class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700"
+  >
+    <form
+      class="space-y-6"
+      action="#"
+    >
+      <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+        Sign in to your dashboard
+      </h3>
+      <button class="relative mt-6 border rounded-md py-2 text-sm text-gray-800 bg-gray-100 hover:bg-gray-200">
+        <span class="absolute left-0 top-0 flex items-center justify-center h-full w-10 text-blue-500"><i class="fab fa-google-f" /></span>
+      </button>
+      <div class="relative mt-10 h-px bg-gray-300">
+        <div class="absolute left-0 top-0 flex justify-center w-full -mt-2">
+          <span class="bg-white px-4 text-xs text-gray-500 uppercase">Or Login With Email</span>
+        </div>
+      </div>
+      <div>
+        <label
+          for="email"
+          class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+        >Your email</label>
+        <input
+          id="email"
+          v-model="email"
+          type="email"
+          name="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          placeholder="name@example.com"
+          required=""
+        >
+      </div>
+      <div>
+        <label
+          for="password"
+          class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+        >Your password</label>
+        <input
+          id="password"
+          v-model="password"
+          type="password"
+          name="password"
+          placeholder="••••••••"
+          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          required=""
+        >
+      </div>
+      <div class="flex items-start">
+        <div class="flex items-start">
+          <div class="flex items-center h-5">
+            <input
+              id="remember"
+              aria-describedby="remember"
+              type="checkbox"
+              class="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-purple-300 h-4 w-4 rounded dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-purple-600 dark:ring-offset-gray-800"
+              required=""
+            >
+          </div>
+          <div class="text-sm ml-3">
+            <label
+              for="remember"
+              class="font-medium text-gray-900 dark:text-gray-300"
+            >Remember me</label>
+          </div>
+        </div>
+        <a
+          href="#"
+          class="text-sm text-purple-700 hover:underline ml-auto dark:text-purple-500"
+        >Forgot
+          Password?</a>
+      </div>
+      <button
+        type="submit"
+        class="w-full text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+        @click="login"
+      >
+        Sign in
+      </button>
+      <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+        Not registered? 
+        <router-link
+          to="/register"
+          class="text-purple-700 hover:underline dark:text-purple-500"
+        >
+          Create account
+        </router-link>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -49,27 +115,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.login {
-  margin-top: 50px;
-}
-input {
-  width: 40%;
-  padding: 20px;
-  margin: 20px 0;
-}
-button {
-  width: 10%;
-  margin-top: 30px;
-  padding: 20px;
-  cursor: pointer;
-}
-p {
-  margin-top: 50px;
-  font-size: 16px;
-}
-p a {
-  cursor: pointer;
-}
-</style>
