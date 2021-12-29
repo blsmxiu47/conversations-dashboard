a340efb55540bb8ce1259a6c7004c05ca004255e
Wes Warriner <fridgehistory@gmail.com>
2021-12-28 22:25:11 -0800

    add placeholder card component for UI testing

---
+++ src/partials/dashboard/PlaceholderCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white shadow-lg rounded-sm border border-gray-200">
+    <span>Placeholder Card</span>
+    <div>and</div>
+    <div>more</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PlaceholderCard'
+}
+</script>
