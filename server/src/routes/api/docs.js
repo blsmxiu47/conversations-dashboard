45cc3aa34da5d5699e0db9e267ee990932da37d8
Wes Warriner <fridgehistory@gmail.com>
2022-01-09 12:22:02 -0800

    add locally working implementation of loadGoogleNewsCollection to GET News items from mongoDB

--- server/src/routes/api/docs.js
+++ server/src/routes/api/docs.js
@@ -1,11 +1,52 @@
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config();
+//   }
+require('custom-env').env('local');
+
 const express = require('express');
-const mongodb = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 const router = express.Router();
 
 // Get Docs
-router.get('/', (req, res) => {
-    res.send('test');
+router.get('/', async (req, res) => {
+    // res.send('test');
+    const docs = await loadGoogleNewsCollection().catch(console.error);
+    res.send(await docs.find({}).toArray());
 });
+
+
+// async function loadDocs() {
+//     const uri = `mongodb+srv://${process.env.MONGO_UN}:${process.env.MONGO_PW}@cluster0.seck8.mongodb.net/conversations?retryWrites=true&w=majority`;
+//     const client = new MongoClient(uri, { 
+//         useNewUrlParser: true, useUnifiedTopology: true 
+//     });
+
+//     try {
+//         // Connect to Mongo cluster
+//         await client.connect();
+
+//         // Load in all GoogleNews docs
+//         return await loadGoogleNewsCollection(client).catch(console.error);
+
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
+
+// async function loadGoogleNewsCollection(client) {
+//     return client.db('conversations').collection('googlenews');
+// }
+
+async function loadGoogleNewsCollection() {
+    const uri = `mongodb+srv://${process.env.MONGO_UN}:${process.env.MONGO_PW}@cluster0.seck8.mongodb.net/conversations?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, { 
+        useNewUrlParser: true, useUnifiedTopology: true 
+    });
+    await client.connect();
+    return client.db('conversations').collection('googlenews');
+}
 
 module.exports = router;
