b859c329e3aca5b734e10342035732bf4bd88d24
Wes Warriner <fridgehistory@gmail.com>
2022-01-09 12:42:59 -0800

    update docs.js with generalizeable collection loading function

--- server/src/routes/api/docs.js
+++ server/src/routes/api/docs.js
@@ -10,43 +10,34 @@ const router = express.Router();
 
 // Get Docs
 router.get('/', async (req, res) => {
-    // res.send('test');
-    const docs = await loadGoogleNewsCollection().catch(console.error);
+    const docs = await loadDocs().catch(console.error);
     res.send(await docs.find({}).toArray());
 });
 
-
-// async function loadDocs() {
-//     const uri = `mongodb+srv://${process.env.MONGO_UN}:${process.env.MONGO_PW}@cluster0.seck8.mongodb.net/conversations?retryWrites=true&w=majority`;
-//     const client = new MongoClient(uri, { 
-//         useNewUrlParser: true, useUnifiedTopology: true 
-//     });
-
-//     try {
-//         // Connect to Mongo cluster
-//         await client.connect();
-
-//         // Load in all GoogleNews docs
-//         return await loadGoogleNewsCollection(client).catch(console.error);
-
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         await client.close();
-//     }
-// }
-
-// async function loadGoogleNewsCollection(client) {
-//     return client.db('conversations').collection('googlenews');
-// }
-
-async function loadGoogleNewsCollection() {
+async function loadDocs() {
     const uri = `mongodb+srv://${process.env.MONGO_UN}:${process.env.MONGO_PW}@cluster0.seck8.mongodb.net/conversations?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, { 
         useNewUrlParser: true, useUnifiedTopology: true 
     });
+
+    // Connect to Mongo cluster
     await client.connect();
-    return client.db('conversations').collection('googlenews');
+
+    // Load in all GoogleNews docs
+    return collection =  loadCollection(client, 'googlenews');
 }
+
+function loadCollection(client, collection) {
+    return client.db('conversations').collection(collection);
+}
+
+// async function loadGoogleNewsCollection() {
+//     const uri = `mongodb+srv://${process.env.MONGO_UN}:${process.env.MONGO_PW}@cluster0.seck8.mongodb.net/conversations?retryWrites=true&w=majority`;
+//     const client = new MongoClient(uri, { 
+//         useNewUrlParser: true, useUnifiedTopology: true 
+//     });
+//     await client.connect();
+//     return client.db('conversations').collection('googlenews');
+// }
 
 module.exports = router;
