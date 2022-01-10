785db2180cffb04d100475af325cb06577c8c861
Wes Warriner <fridgehistory@gmail.com>
2022-01-10 09:14:19 -0800

    add axios docservice boilerplate

---
+++ src/service/docservice.js
@@ -0,0 +1,24 @@
+import axios from axios;
+
+const url = 'http://localhost:5000/api/docs/';
+
+class DocService {
+    static getDocs() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(doc => ({
+                        ...doc,
+                        createdAt: new Date(doc.createdAt)
+                    }))
+                );
+            } catch(err) {
+                reject();
+            }
+        });
+    }
+}
+
+export default DocService;
